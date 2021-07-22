1c2909e5249ea206e4ffff43534a11cafcb6a60a
winnie tongle <winnie.carole.tongle.djoukeng@mni.thm.de>
2021-07-22 21:02:38 +0200

    add comentare

--- app.js
+++ app.js
@@ -5,36 +5,38 @@ const bodyparser=require("body-parser");
 app.use(bodyparser.urlencoded({extended: true}))
 
 
-
+/*Hier wird der Html file gelesen*/
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
    /* 
     */
     
 })
+/* es wird die data and der appi gesendet */
 app.post("/",function(req,res){
     const city = req.body.cityName;
     const apikey="353695c45eac57b7362f129c86f212fc";
     const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apikey
     https.get(url,function(response){
        
-        console.log(response.statusCode);
+       // console.log(response.statusCode);
 
         response.on("data",function(data){
             const weatherdata=JSON.parse(data);
-            console.log(weatherdata);
+            //console.log(weatherdata);
             const temp =weatherdata.main.temp;
             const icon=weatherdata.weather[0].icon;
             const imageURL="http://openweathermap.org/img/wn/"+ icon +"@2x.png";
             const weatherDescription=weatherdata.weather[0].description;
            res.write("<p>The weather is currently "+weatherDescription+ "<p>");
-           res.write("<h1>the temperature in "+query +" is "+temp+"degree celcuis </h1>");
+           res.write("<h1>the temperature in "+city +" is "+temp+"kelvin </h1>");
            res.write("<img src="+ imageURL+">")
            res.send();
         })
     })
 })
 
+/*es wird hier ein port für der app gewählt*/
 app.listen(2004,function(){
     console.log("app listen on port 2004");
 })
