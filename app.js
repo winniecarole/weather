db23b7c17a0c8f6180f552c1c5274f1115e8b7ad
winniecarole <winniedjoukeng@gmail.com>
2021-08-25 01:03:52 +0200

    modified url

--- app.js
+++ app.js
@@ -8,32 +8,31 @@ app.use(bodyparser.urlencoded({extended: true}))
 /*Hier wird der Html file gelesen*/
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
-   /* 
-    */
-    
+  
 })
 /* es wird die data and der appi gesendet */
 app.post("/",function(req,res){
     const city = req.body.cityName;
     const apikey="353695c45eac57b7362f129c86f212fc";
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apikey
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apikey+ "&units=metric"
+    
     https.get(url,function(response){
        
        // console.log(response.statusCode);
-
         response.on("data",function(data){
             const weatherdata=JSON.parse(data);
             //console.log(weatherdata);
             const temp =weatherdata.main.temp;
+            console.log(temp);
             const icon=weatherdata.weather[0].icon;
             const imageURL="http://openweathermap.org/img/wn/"+ icon +"@2x.png";
             const weatherDescription=weatherdata.weather[0].description;
            res.write("<p>The weather is currently "+weatherDescription+ "<p>");
-           res.write("<h1>the temperature in "+city +" is "+temp+"kelvin </h1>");
+           res.write("<h1>the temperature in "+city +" is "+temp+" </h1>");
            res.write("<img src="+ imageURL+">")
            res.send();
-        })
-    })
+        });
+    });
 })
 
 /*es wird hier ein port für der app gewählt*/
